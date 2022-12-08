b72ea85f345226741f506258265eb646cc60620f
notzhong <notzhong@163.com>
2022-12-09 00:08:29 +0800

    修复输出数组文件从第二行起的第一个字符出现0x0的错误。
    Fixed the first character of the output array file from the second line appearing 0x0.

--- HexToBytesFile/main.cpp
+++ HexToBytesFile/main.cpp
@@ -1,10 +1,16 @@
 #include<iostream>
+#include<fstream>
+
+
+#define MAX_READ_BYTE 16
+
 
 int main(int argv, char** args) {
 
     if (argv != 2)
         return argv;
-    FILE* pFileRead, * pFileWite;
+    FILE* pFileRead;
+    std::fstream pFileWrite;
     printf_s("二进制文件转十六进制数组工具开始生效：\n");
 
     printf_s("转换文件为：%s\n", args[1]);
@@ -13,30 +19,41 @@ int main(int argv, char** args) {
     char NewFile[_MAX_PATH]{ 0 };
     sprintf_s(NewFile, _MAX_PATH, "%s.txt", args[1]);
     printf_s("输出文件为:%s\n", NewFile);
-    fopen_s(&pFileWite, NewFile, "w");
 
-    unsigned char Readbuff[17]{ 0 };
+    pFileWrite.open(NewFile, std::ios::out | std::ios::trunc | std::ios::binary);
+    //fopen_s(&pFileWite, NewFile, "wb");
+
+    unsigned char Readbuff[18]{ 0 };
     unsigned char Writebuff[8]{ 0 };
 
     auto size = sizeof(Readbuff);
     while (true)
     {
-        auto GetNum = fread_s(Readbuff, sizeof(Readbuff), 1, 16, pFileRead);
+        auto nGetNum = fread_s(Readbuff, sizeof(Readbuff), 1, MAX_READ_BYTE, pFileRead);
         auto str = Readbuff;
-        for (unsigned int i = 0; i < GetNum; str++, i++)
+        for (unsigned int i = 0, nWrite = 0; i < nGetNum; str++, i++)
         {
-            auto nWrite = sprintf_s(reinterpret_cast<char* const>(Writebuff), sizeof(Writebuff), "0x%X, ", *str);
+           /* if (feof(pFileRead) && i == nGetNum - 1)
+                nWrite = sprintf_s(reinterpret_cast<char* const>(Writebuff), sizeof(Writebuff), "0x%X", *str);
+            else*/
+                nWrite = sprintf_s(reinterpret_cast<char* const>(Writebuff), sizeof(Writebuff), "0x%X, ", *str);
 
-            fwrite(Writebuff, nWrite, 1, pFileWite);
+            pFileWrite << Writebuff;
+            //fwrite(Writebuff, nWrite, 1, pFileWite);
         }
+
         if (!feof(pFileRead))
-            fwrite("\r", sizeof("\r"), 1, pFileWite);
+        {
+            pFileWrite << "\r";
+            //fwrite("\r", sizeof("\r"), 1, pFileWite);
+        }
         else
             break;
     }
 
     fclose(pFileRead);
-    fclose(pFileWite);
+    pFileWrite.close();
+    //fclose(pFileWite);
     printf_s("二进制文件转十六进制数组工具执行完毕！\n");
     return argv;
 }
